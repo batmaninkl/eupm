79c17ea3b5b6b1c2b9d40eed6f4331c4af20e60d
batmaninkl <adamramlan@gmail.com>
2022-01-24 14:14:18 +0800

    Added pillars and styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,52 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+// Video Modal
+$(document).ready(function () {
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+    console.log($videoSrc);
+  });
+  console.log($videoSrc);
+
+  $("#vModal-1").on("shown.bs.modal", function (e) {
+    $("#video-1").attr(
+      "src",
+      $videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1"
+    );
+  });
+
+  $("#vModal-1").on("hide.bs.modal", function (e) {
+    $("#video-1").attr("src", $videoSrc);
+  });
+
+  $("#vModal-2").on("shown.bs.modal", function (e) {
+    $("#video-2").attr(
+      "src",
+      $videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1"
+    );
+  });
+
+  $("#vModal-2").on("hide.bs.modal", function (e) {
+    $("#video-2").attr("src", $videoSrc);
+  });
+
+  $("#vModal-3").on("shown.bs.modal", function (e) {
+    $("#video-3").attr(
+      "src",
+      $videoSrc + "?rel=0&showinfo=0&modestbranding=1&autoplay=1"
+    );
+  });
+
+  $("#vModal-3").on("hide.bs.modal", function (e) {
+    $("#video-3").attr("src", $videoSrc);
+  });
+});
+
+
+
+
 var myCarousel = document.querySelector('#carousel1')
 var carousel = new bootstrap.Carousel(carousel1, {
   interval: 5000,
