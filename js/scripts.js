83023886286bdf985161ee28c033b6f6664d42e4
batmaninkl <adamramlan@gmail.com>
2022-01-25 23:17:38 +0800

    Slider revert + Added articles + added news

--- js/scripts.js
+++ js/scripts.js
@@ -52,9 +52,9 @@ $(document).ready(function () {
 
 
 
-var myCarousel = document.querySelector('#carousel1')
-var carousel = new bootstrap.Carousel(carousel1, {
-  interval: 5000,
-  wrap: true,
-  keyboard: true
-})
+// var myCarousel = document.querySelector('#carousel1')
+// var carousel = new bootstrap.Carousel(carousel1, {
+//   interval: 5000,
+//   wrap: true,
+//   keyboard: true
+// })
